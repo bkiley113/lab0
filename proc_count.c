ba91786e9658900195d601ac4e3330a78f53924f
Brandon Kiley <b.kiley.113@gmail.com>
2025-01-18 22:26:07 -0800

    first attempt

--- proc_count.c
+++ proc_count.c
@@ -4,16 +4,29 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
+//pointer to a proc_dir_entry data structure
 static struct proc_dir_entry *entry;
 
 static int proc_count(struct seq_file *m, void *v){
-	// TODO: it's all yours
+	
+	int count;
+	struct task_struct *task;
+
+	for_each_process(task){
+		count++
+	}
+
+	seq_printf(m, "Process count is %d\n", count);
 	return 0;
 }
 
+
 static int __init proc_count_init(void)
 {
+	//here is our proc_dir_entry data structure
 	entry = proc_create_single("count", 0, NULL, proc_count);
+
+	//log kernel message with 'info' severity level
 	pr_info("proc_count: init\n");
 	return 0;
 }
